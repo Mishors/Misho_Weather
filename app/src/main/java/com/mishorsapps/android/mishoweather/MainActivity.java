32b9fb4047287c659800fa7235f41e3173996c5b
Mishors <mishors@yahoo.co.uk>
2017-08-08 01:43:12 +0200

    After deleting some not used variables, classes and methods

--- app/src/main/java/com/mishorsapps/android/mishoweather/MainActivity.java
+++ app/src/main/java/com/mishorsapps/android/mishoweather/MainActivity.java
@@ -1,10 +1,10 @@
 package com.mishorsapps.android.mishoweather;
 
 import android.Manifest;
-import android.content.Context;
+
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.location.Criteria;
+
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -14,23 +14,17 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
+
 
 
 
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "Mains";
-    private static final int MY_PERMISSION_REQUEST_FINE_LOCATION = 101;
-    private static final int MY_PERMISSION_REQUEST_COARSE_LOCATION = 102;
-    private boolean permissionIsGranted = false;
-    static TextView temperatureTV;
-    static TextView cityTV;
-    
+
     private Button b;
     private LocationManager locationManager;
     private LocationListener listener;
@@ -38,14 +32,10 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.d(TAG, "Start!");
+
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-
-       // temperatureTV = (TextView) findViewById(R.id.temperatureTextView);
-
-       // cityTV = (TextView) findViewById(R.id.cityNameTextView);
 
         setContentView(R.layout.activity_main);
 
@@ -126,50 +116,4 @@ public class MainActivity extends AppCompatActivity {
         }
     }//method
 
-    public void getWeather(){
-        //Get the Device current Location via Android Location
-
-        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-
-        String provider = locationManager.getBestProvider(new Criteria(), false);
-
-         /*Permission Check*/
-        //  if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-        //      Log.d(TAG,"Before Return");
-        //     return;
-        // }
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSION_REQUEST_FINE_LOCATION);
-                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, MY_PERMISSION_REQUEST_COARSE_LOCATION);
-            }//if
-            else{
-                permissionIsGranted = true;
-            }
-            return;
-        }
-
-        if(permissionIsGranted) {
-            Location location = locationManager.getLastKnownLocation(provider);
-
-            double lat = location.getLatitude();
-
-            double lon = location.getLongitude();
-
-            //Perform the asyncTask to download weather from the API
-
-            DownloadTask task = new DownloadTask();
-
-            // task.execute("http://api.openweathermap.org/data/2.5/weather?lat=" + String.valueOf(lat) + "&lon=" + String.valueOf(lon) + "&appid=6e911705fe7d9ce91b7a63f2aefc9013");
-
-            task.execute("http://api.openweathermap.org/data/2.5/weather?q=London&appid=6e911705fe7d9ce91b7a63f2aefc9013");
-        }//if
-    }//method
 }//class
